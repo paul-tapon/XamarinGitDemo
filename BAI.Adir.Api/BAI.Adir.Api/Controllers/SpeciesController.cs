afa872faabab1931955dfc08caf6904413b29180
Andy <ajcarbues@gmail.com>
2020-10-21 12:57:02 +0800

    added species controller.

---
+++ BAI.Adir.Api/BAI.Adir.Api/Controllers/SpeciesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BAI.Adir.Api.Domain.Context;
+using BAI.Adir.Api.Domain.Model;
+
+namespace BAI.Adir.Api.Controllers
+{
+    public class SpeciesController : ApiController
+    {
+        private AdirContext db = new AdirContext();
+
+        // GET: api/Species
+        public IQueryable<Species> GetSpecies()
+        {
+            return db.Species;
+        }
+
+        // GET: api/Species/5
+        [ResponseType(typeof(Species))]
+        public IHttpActionResult GetSpecies(int id)
+        {
+            Species species = db.Species.Find(id);
+            if (species == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(species);
+        }
+
+        // PUT: api/Species/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSpecies(int id, Species species)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != species.SpeciesId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(species).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SpeciesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Species
+        [ResponseType(typeof(Species))]
+        public IHttpActionResult PostSpecies(Species species)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Species.Add(species);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = species.SpeciesId }, species);
+        }
+
+        // DELETE: api/Species/5
+        [ResponseType(typeof(Species))]
+        public IHttpActionResult DeleteSpecies(int id)
+        {
+            Species species = db.Species.Find(id);
+            if (species == null)
+            {
+                return NotFound();
+            }
+
+            db.Species.Remove(species);
+            db.SaveChanges();
+
+            return Ok(species);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SpeciesExists(int id)
+        {
+            return db.Species.Count(e => e.SpeciesId == id) > 0;
+        }
+    }
+}
