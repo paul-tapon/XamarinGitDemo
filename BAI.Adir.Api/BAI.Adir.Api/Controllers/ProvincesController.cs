e32e198351a2badc8480da0f100f8b0a7e54d8c5
Paul Tapon <paultapon@gmail.com>
2020-10-21 13:10:11 +0800

    added region,province,municipality and barangay controller

---
+++ BAI.Adir.Api/BAI.Adir.Api/Controllers/ProvincesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BAI.Adir.Api.Domain.Context;
+using BAI.Adir.Api.Domain.Model;
+
+namespace BAI.Adir.Api.Controllers
+{
+    [RoutePrefix("provinces")]
+    public class ProvincesController : ApiController
+    {
+        private AdirContext db = new AdirContext();
+
+        // GET: api/Provinces
+        public IQueryable<Province> GetProvinces()
+        {
+            return db.Provinces;
+        }
+
+        // GET: api/Provinces
+        [Route("GetProvinceByRegionId")]
+        [HttpGet]
+        public IHttpActionResult GetProvinceByRegionId(int id)
+        {
+            return Ok(db.Provinces.Where(p=>p.RegionId == id));
+        }
+
+        // GET: api/Provinces/5
+        [ResponseType(typeof(Province))]
+        public IHttpActionResult GetProvince(int id)
+        {
+            Province province = db.Provinces.Find(id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(province);
+        }
+
+        // PUT: api/Provinces/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProvince(int id, Province province)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != province.ProvinceId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(province).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProvinceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Provinces
+        [ResponseType(typeof(Province))]
+        public IHttpActionResult PostProvince(Province province)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Provinces.Add(province);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = province.ProvinceId }, province);
+        }
+
+        // DELETE: api/Provinces/5
+        [ResponseType(typeof(Province))]
+        public IHttpActionResult DeleteProvince(int id)
+        {
+            Province province = db.Provinces.Find(id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            db.Provinces.Remove(province);
+            db.SaveChanges();
+
+            return Ok(province);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProvinceExists(int id)
+        {
+            return db.Provinces.Count(e => e.ProvinceId == id) > 0;
+        }
+    }
+}
