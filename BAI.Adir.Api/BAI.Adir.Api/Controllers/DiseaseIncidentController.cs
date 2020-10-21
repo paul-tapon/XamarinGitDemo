6f07bec2e4ffe5c81f1bbb49f9f36ebb41213582
Enzo Navarro <johnlorenznavarro@gmail.com>
2020-10-21 13:13:17 +0800

    Added Incident List

---
+++ BAI.Adir.Api/BAI.Adir.Api/Controllers/DiseaseIncidentController.cs
@@ -0,0 +1,39 @@
+﻿using BAI.Adir.Api.Domain.Context;
+using BAI.Adir.Api.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+
+namespace BAI.Adir.Api.Controllers
+{
+    public class DiseaseIncidentController : ApiController
+    {
+        public List<DiseaseIncident> Get()
+        {
+            var context = new AdirContext();
+            return context.DiseaseIncidents.ToList();
+        }
+
+        [Route("api/DiseaseIncident/filter")]
+
+        public IHttpActionResult Get(string region,string province, string municipality, string barangay, string species, string sortField, string sortDir)
+        {
+            var context = new AdirContext();
+
+            IQueryable<DiseaseIncident>
+                query = context.DiseaseIncidents;
+
+            if (!string.IsNullOrEmpty(region))
+            {
+                query = query.Where(p => p.Barangay.Municipality.Province.Region.Name == region);
+                    
+            }
+            return Ok(query.ToList());
+        }
+    }
+}
