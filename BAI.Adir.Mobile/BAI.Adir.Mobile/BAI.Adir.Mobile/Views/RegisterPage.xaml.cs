9d3d19483e6530daf246a5f1cbe337a5bde79dea
Kevin <userbypass2017@gmail.com>
2020-10-22 17:39:04 +0800

    register page

--- BAI.Adir.Mobile/BAI.Adir.Mobile/BAI.Adir.Mobile/Views/RegisterPage.xaml.cs
+++ BAI.Adir.Mobile/BAI.Adir.Mobile/BAI.Adir.Mobile/Views/RegisterPage.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,50 +17,143 @@ namespace BAI.Adir.Mobile.Views
 {
     public partial class RegisterPage : ContentPage
     {
-        string apiUrl = "https://webapidemo-ls8.conveyor.cloud/api/";
-        public AppUser appUser { get; set; }
+        string apiUrl = "https://bai-adir-api.conveyor.cloud/api/";
+        public RegisterDto registerDto { get; set; }
+        bool _isSaving { get; set; }
+        RegisterViewModel viewModel = null;
+
         public RegisterPage()
         {
             InitializeComponent();
-            BindingContext = new RegisterViewModel();
+            viewModel = new RegisterViewModel();
+            BindingContext = viewModel;
+            _isSaving = true;
+            ValidateSave();
         }
 
-        private async Task buttonSave_Clicked(object sender, EventArgs e)
+        private void isSavingForm(bool issaving)
         {
-            var client = new RestClient(apiUrl);
-            var request = new RestRequest("appUsers", DataFormat.Json);
+            buttonSave.IsEnabled = issaving;
+
+            _ = issaving == true ? buttonSave.Text = "Save" : buttonSave.Text = "Saving...";
+            entryEmail.IsEnabled = issaving;
+            entryFirstName.IsEnabled = issaving;
+            entryLastName.IsEnabled = issaving;
+            entryMiddleName.IsEnabled = issaving;
+            entryPassword.IsEnabled = issaving;
+            entryRepeatPassword.IsEnabled = issaving;
+            entryUsername.IsEnabled = issaving;
+            OnPropertyChanged("_isSaving");
+
+        }
+        public class Validator
+        {
+            public static bool EmailIsValid(string emailaddress)
+            {
+                try
+                {
+                    MailAddress mail = new MailAddress(emailaddress);
+                    return true;
+                }
+                catch (FormatException)
+                {
+                    return false;
+                }
+
+            }
+        }
+        private void ValidateSave()
+        {
+            if (!String.IsNullOrWhiteSpace(entryFirstName.Text)
+                && !String.IsNullOrWhiteSpace(entryMiddleName.Text)
+                && !String.IsNullOrWhiteSpace(entryLastName.Text)
+                && !String.IsNullOrWhiteSpace(entryUsername.Text)
+                && !String.IsNullOrWhiteSpace(entryPassword.Text)
+                && !String.IsNullOrWhiteSpace(entryRepeatPassword.Text)
+                && !String.IsNullOrWhiteSpace(entryEmail.Text))
+            {
+                buttonSave.IsEnabled = true;
+            }
+            else
+            {
+                buttonSave.IsEnabled = false;
+            }
+        }
+        protected bool checkEmail(string email)
+        {
+            if (!string.IsNullOrEmpty(email))
+            {
+                if (Validator.EmailIsValid(email))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private async void buttonSave_Clicked(object sender, EventArgs e)
+        {
+            //isSavingForm(!_isSaving);
+
+            //if (!checkEmail(entryEmail.Text))
+            //{
+            //    await DisplayAlert("EMAIL", "Email not in correct format", "OK");
+            //    //await Shell.Current.GoToAsync("//LoginPage");
+            //    isSavingForm(_isSaving);
+            //    return;
+            //}
+            //var client = new RestClient(apiUrl);
+            //var request = new RestRequest("appUsers", DataFormat.Json);
             //var context = new AppUser();
 
-            OnPropertyChanged("IsBusy");
-            try
-            {
-                request.AddJsonBody(appUser);
-                request.Method = Method.POST;
+            ////OnPropertyChanged("IsBusy");
+            //try
+            //{
+            //    context.FirstName = entryFirstName.Text;
+            //    context.MiddleName = entryMiddleName.Text;
+            //    context.LastName = entryLastName.Text;
+            //    context.Username = entryUsername.Text;
+            //    context.PasswordHash = entryPassword.Text;
+            //    context.Email = entryEmail.Text;
 
-                var response = await client.ExecuteAsync(request);
-                var productData = JsonConvert.DeserializeObject<AppUser>(response.Content);
+            //    //request.AddQueryParameter
+            //    request.AddJsonBody(context);
+            //    request.Method = Method.POST;
 
-                if (response.StatusCode == HttpStatusCode.Created)
-                {
-                    //await UploadPhoto(productData.ProductID);
-                    //await DisplayAlert("System Message", response.Content, "OK");
-                    await DisplayAlert("Success", response.Content, "OK");
-                    //await Shell.Current.GoToAsync("//LoginPage");
-                }
-                else
-                {
-                    await DisplayAlert("Error", response.Content, "NOT OK");
-                    //await Shell.Current.GoToAsync("//LoginPage");
-                }
-            }
-            catch (Exception ex)
-            {
+            //    var response = await client.ExecuteAsync(request);
+            //    //var productData = JsonConvert.DeserializeObject<AppUser>(response.Content);
 
-            }
-            finally
+            //    if (response.StatusCode == HttpStatusCode.Created)
+            //    {
+            //        //await UploadPhoto(productData.ProductID);
+            //        //await DisplayAlert("System Message", response.Content, "OK");
+            //        await DisplayAlert("Success", "Please check your email for verification", "OK");
+            //        //await Shell.Current.GoToAsync("//LoginPage");
+            //        isSavingForm(_isSaving);
+            //    }
+            //    else
+            //    {
+            //        await DisplayAlert("Error", "API not success", "OK");
+            //        //await Shell.Current.GoToAsync("//LoginPage");
+            //        isSavingForm(_isSaving);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+            //finally
+            //{
+            //    //IsBusy = false;
+            //    //OnPropertyChanged("IsBusy");
+            //}
+        }
+        private async void EntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(e.NewTextValue !=null)
             {
-                IsBusy = false;
-                OnPropertyChanged("IsBusy");
+                var entry = sender as Entry;
+                ValidateSave();
             }
         }
     }
